157c8eff054e8fe9e8dbdbb7fd33c15a6b92f535
Agnik Bakshi <bakshi.agnik@gmail.com>
2024-01-07 09:12:54 +0530

    Project Setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "primary-light-red": "hsl(0, 100%, 67%)",
+        "primary-orange-yellow": "hsl(39, 100%, 56%)",
+        "primary-green-teal": "hsl(166, 100%, 37%)",
+        "primary-cobalt-blue": "hsl(234, 85%, 45%)",
+        "grad-slate-blue": "hsl(252, 100%, 67%)",
+        "grad-royal-blue": "hsl(241, 81%, 54%)",
+        "grad-violet-blue": "hsla(256, 72%, 46%, 1)",
+        "grad-persian-blue": "hsla(241, 72%, 46%, 0)",
+        "neutral-pale-blue": "hsl(221, 100%, 96%)",
+        "neutral-light-lavender": "hsl(241, 100%, 89%)",
+        "neutral-dark-gray-blue": "hsl(224, 30%, 27%)"
+      },
+      fontFamily:{
+        "hanken": ["Hanken Grotesk", "sans-serif"]
+      }
+    },
   },
   plugins: [],
 }
